00c68519de21a752001f91a516de864f32fa138b
Devina Kumar <devina@devinas-mbp.wireless.dartmouth.edu>
2017-05-13 18:32:27 -0400

    working on editor

--- editor.py
+++ editor.py
@@ -5,6 +5,7 @@ from datetime import datetime                # get datetime
 import mysql.connector                       # mysql functionality
 import sys                                   # for misc errors
 
+
 class Editor:
     def __init__(self, id, connection):
         self.id = id
@@ -56,25 +57,31 @@ class Editor:
 
         cursor.close()
 
-
     def assign(self, manuscriptId, reviewerId):
         # check if reviewer is retired
-        query = "SELECT Retired, RICode FROM Reviewer WHERE ReviewerId = %d" % (reviewerId)
-        cursor = self.con.cursor()
+        query = "SELECT Retired FROM Reviewer WHERE ReviewerId = %d" % (reviewerId)
+        cursor = self.con.cursor(buffered=True)
         cursor.execute(query)
         resultReviewer = cursor.fetchone()
-        if result[0] == 1:
+        if resultReviewer[0] == 1:
             print("Sorry, but this reviewer is retired.  Please assign this manuscript to someone else.")
             cursor.close()
             return
         cursor.close()
         print("first stage of assign done")
+
         # check if manuscript RI and reviewer RI match
-        query2 = "SELECT RICode FROM Manuscript WHERE ReviewerId = %d" % (reviewerId)
-        cursor2 = self.con.cursor()
+        query5 = "SELECT RICode FROM ReviewerInterests WHERE ReviewerId = %d" % (reviewerId)
+        cursor5 = self.con.cursor(buffered=True)
+        cursor5.execute(query5)
+        resultRI = cursor5.fetchone()
+        cursor5.close()
+
+        query2 = "SELECT RICode FROM Manuscript WHERE ManuscriptId = %d" % (manuscriptId)
+        cursor2 = self.con.cursor(buffered=True)
         cursor2.execute(query2)
         resultManuscript = cursor2.fetchone()
-        if resultReviewer[1] != resultManuscript[0]:
+        if resultRI[0] != resultManuscript[0]:
             print("Sorry, but this reviewer does not review manuscripts in this field.  Please assign this manuscript to another reviewer.")
             cursor2.close()
             return
@@ -83,13 +90,13 @@ class Editor:
         # if they match, assign a manuscript by creating a review assigned to the reviewer with a timestamp
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         query3 = "INSERT INTO Review (ManuscriptId, ReviewerId, DateSent) VALUES (%d, %d, '%s')" % (manuscriptId, reviewerId, timestamp)
-        cursor3 = self.con.cursor()
+        cursor3 = self.con.cursor(buffered=True)
         cursor3.execute(query3)
         self.con.commit()
         cursor3.close()
 
         # change manuscript status to Under Review
-        cursor4 = self.con.cursor()
+        cursor4 = self.con.cursor(buffered=True)
         query4 = "UPDATE Manuscript SET Status='%s' WHERE ManuscriptId=%d" % ('Under Review', manuscriptId)
         cursor4.execute(query4)
         self.con.commit()
